83a6d9648a2ee43633f7f3b8419395e66b01b481
dsarfo <59879283+dsarfo@users.noreply.github.com>
2020-03-19 20:25:09 -0400

    Components initial commit

---
+++ force-app/main/default/aura/PanelListView/PanelListViewController.js
@@ -0,0 +1,15 @@
+({
+    handleSearchNameID : function(component, event, helper) {
+		/*the search querry is handle by the helper controller 
+		 * hence handleSearchNameID method just invoke the helper controller */ 
+        helper.findNameOrID(component, event, helper);
+    },
+    handleRowAction: function(component, event, helper) {
+         var action = event.getParam('action');
+        if(action.name=='show_details')
+            component.set("v.ShowRecordDetails",True);
+            else
+                component.set("v.ShowRecordDetails",False);
+        
+    }
+})
